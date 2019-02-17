3f7d7087b56a6adcbc17ed676da16ffd6cdb9a83
zbrsnd <zbrsnd@gmail.com>
2019-02-17 22:25:01 +0100

    dodanie widoku strony glownej

--- uczniowie/views.py
+++ uczniowie/views.py
@@ -13,3 +13,31 @@ def index():
     """Strona główna"""
     return render_template('index.html')
 
+@app.route('/lista_ucz')
+def lista_ucz():
+    uczniowie = Uczen.select()
+    return render_template('lista_uczniow.html', query=uczniowie)
+    
+@app.route('/lista_kl')
+def lista_kl():
+    klasy = Klasa.select()
+    return render_template('lista_klas.html', query=klasy)
+    
+    
+@app.route('/dodaj_kl', methods=['GET', 'POST'])
+def dodaj_kl():
+    form = DodajForm()
+    form.klasa.choices = [(k.id, k.klasa) for k in Klasa.select()]
+    
+    if form.validate_on_submit():
+        print(form.data)
+        k = Klasa(klasa=form.klasa.data, rok_naboru=form.rok_naboru.data, rok_matury=form.rok_matury.data)
+        k.save()
+        
+        flash("Dodano klasę!", "sukces")
+        return redirect(url_for('index'))
+    elif request.method == 'POST':
+        flash_errors(form)
+        
+    return render_template('dodaj_klasa.html', form=form)
+
